26724fe8f55c9233a3dafb478254a14eb951706a
l0calh05t <l0calh05t@gmx.net>
2021-12-06 21:09:02 +0100

    Day 6 O(1) algorithm

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,4 +1,6 @@
 use color_eyre::Result;
+use ndarray::prelude::*;
+use ndarray_linalg::{types::c64, AsDiagonal, Eig, Inverse, LinearOperator};
 use std::{
 	fs::File,
 	io::{BufRead, BufReader},
@@ -21,19 +23,53 @@ fn main() -> Result<()> {
 		assert!((0..=6).contains(&timer));
 		timer_counts[timer as usize] += 1;
 	}
-	let iterate = |timer_counts: &mut [usize; 9]| {
-		// shift every timer one to the left, 0 is mapped to 8
-		timer_counts.rotate_left(1);
-		// every timer that was in 0 should also be added to 6
-		timer_counts[6] += timer_counts[8];
-	};
-	for _ in 0..80 {
-		iterate(&mut timer_counts);
-	}
-	println!("{}", timer_counts.iter().sum::<usize>());
-	for _ in 80..256 {
-		iterate(&mut timer_counts);
-	}
-	println!("{}", timer_counts.iter().sum::<usize>());
+
+	let iteration_matrix = arr2(&[
+		[0., 1., 0., 0., 0., 0., 0., 0., 0.],
+		[0., 0., 1., 0., 0., 0., 0., 0., 0.],
+		[0., 0., 0., 1., 0., 0., 0., 0., 0.],
+		[0., 0., 0., 0., 1., 0., 0., 0., 0.],
+		[0., 0., 0., 0., 0., 1., 0., 0., 0.],
+		[0., 0., 0., 0., 0., 0., 1., 0., 0.],
+		[1., 0., 0., 0., 0., 0., 0., 1., 0.],
+		[0., 0., 0., 0., 0., 0., 0., 0., 1.],
+		[1., 0., 0., 0., 0., 0., 0., 0., 0.],
+	]);
+
+	let (eigenvalues, eigenvectors) = iteration_matrix.eig()?;
+	let eigenvectors_inv = eigenvectors.inv()?;
+
+	// compute Re{P D⁸⁰ P⁻¹}x = M⁸⁰x
+	let result_80 = eigenvectors
+		.apply2(
+			&eigenvalues
+				.mapv(|e: c64| e.powc(80.0.into()))
+				.as_diagonal()
+				.apply2(&eigenvectors_inv),
+		)
+		.view()
+		.split_complex()
+		.re
+		.mapv(|e| e.round() as usize)
+		.dot(&ArrayView::from(&timer_counts))
+		.sum();
+	println!("{}", result_80);
+
+	// compute Re{P D²⁵⁶ P⁻¹}x = M²⁵⁶x
+	let result_256 = eigenvectors
+		.apply2(
+			&eigenvalues
+				.mapv(|e: c64| e.powc(256.0.into()))
+				.as_diagonal()
+				.apply2(&eigenvectors_inv),
+		)
+		.view()
+		.split_complex()
+		.re
+		.mapv(|e| e.round() as usize)
+		.dot(&ArrayView::from(&timer_counts))
+		.sum();
+	println!("{}", result_256);
+
 	Ok(())
 }
